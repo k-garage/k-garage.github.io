89e8064bce7c3218cd25435232b30f0032bb9bf6
SimoRyu <cloneofsimo@korea.ac.kr>
2022-11-25 17:03:51 +0000

    built

---
+++ public/katex.js
@@ -0,0 +1,20 @@
+import renderMathInElement from "https://cdn.jsdelivr.net/npm/katex@0.15.2/dist/contrib/auto-render.mjs";
+window.addEventListener("pageshow", function (event) {
+  var historyTraversal =
+    event.persisted ||
+    (typeof window.performance != "undefined" &&
+      window.performance.navigation.type === 2);
+  if (historyTraversal) {
+    // Handle page restore.
+    window.location.reload();
+  }
+});
+renderMathInElement(document.body, {
+  delimiters: [
+    { left: "$$", right: "$$", display: true },
+    { left: "$", right: "$", display: false },
+    { left: "\\(", right: "\\)", display: false },
+    { left: "\\[", right: "\\]", display: true },
+  ],
+  strict: "ignore",
+});
